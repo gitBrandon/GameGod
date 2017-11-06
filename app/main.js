a28a1bad9f3e32bf71a503c2564d478c110bde2d
Unknown <brandon@devit.co.za>
2017-11-06 16:30:45 +0200

    Added game blocks

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 function Main() {
 
-    var supportedGameArray = [730, 570];
+    var supportedGameArray = [730, 570, 578080];
 
     function Initialise() {
         $(supportedGameArray).each(function(idx, el) {
@@ -14,17 +14,30 @@ function Main() {
             type: 'GET',
             dataType : 'JSON',
             success: function(data) {
-                callback(data);
+                callback(data, gameid);
             }
         });
-
-        function setHeader() {
-        	xhr.setRequestHeader('Authorization', token);
-        }
     }
 
-    function BuildGameBlocks(data) {
-        console.log(data);
+    function BuildGameBlocks(data, gameid) {
+        var tmpJSON = JSON.parse(data);
+        var gameData = tmpJSON[gameid];
+
+        console.log(gameData);
+
+        var gameName = gameData.data.name;
+        var gameImage = gameData.data.header_image;
+
+        var html = `
+        <div class="col-sm-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">${gameName}</div>
+                <div class="panel-body"><img src="${gameImage}" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer"></div>
+            </div>
+        </div>`;
+
+        $("#gameHolder").append(html)
     }
 
     return {
